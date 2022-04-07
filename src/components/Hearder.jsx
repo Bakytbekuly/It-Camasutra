9f9f73f0e98703b818a07ca31e813b96010b0df5
Ainur <mr.rustem071193@gmail.com>
2022-04-08 03:51:54 +0600

    it Camasutra

---
+++ src/components/Hearder.jsx
@@ -0,0 +1,9 @@
+
+function Header() {
+    return (
+        <header className='header'>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Vanamo_Logo.svg/900px-Vanamo_Logo.svg.png" alt="" />
+        </header>)
+}
+
+export default Header
---
+++ src/components/Nav.jsx
@@ -0,0 +1,24 @@
+function Nav() {
+    return (
+        <nav className='nav'>
+            <div>
+                Profaile
+            </div>
+            <div>
+                Messages
+            </div>
+            <div>
+                News
+            </div>
+            <div>
+                Music
+            </div>
+            <div>
+                Settings
+            </div>
+        </nav>
+
+    )
+}
+
+export default Nav
